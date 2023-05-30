714f45126444daaffb06f437d92bfb5a5b67262f
Indriaaaa24 <108176162+Indriaaaa24@users.noreply.github.com>
2023-05-30 15:03:25 +0700

    Add files via upload

---
+++ Cloud Computing/serverAPI.py
@@ -0,0 +1,94 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from uuid import uuid4
+from datetime import datetime, timedelta
+
+from tensorflow import keras
+model = keras.models.load_model('acne_model.h5')
+
+import jwt
+import secrets
+import string
+
+db = SQLAlchemy()
+
+def CreateUUID():
+    return uuid4().hex
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.String(32), unique=True, primary_key=True, default=CreateUUID)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(), nullable=False)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/database-api'
+app.config['SECRET_KEY'] = 'secret'
+db.init_app(app)
+
+@app.route('/')
+def hello():
+    return 'successful'
+
+@app.route("/register", methods=["POST"])
+def RegisterUser():
+    try:
+        user_data = request.get_json()
+        email = user_data["email"]
+        password = user_data["password"]
+
+        hashed_password = generate_password_hash(password)
+
+        user = User(email=email, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return "success", 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"error": "Invalid email"}), 401
+
+    if not check_password_hash(user.password, password):
+        return jsonify({"error": "Invalid password"}), 401
+
+    token = jwt.encode({
+        'username': email,
+        'exp': datetime.utcnow() + timedelta(minutes=30)},  
+        app.config['SECRET_KEY'],
+        algorithm='HS256'
+    )
+
+    return jsonify({"token": token}), 201
+
+@app.route("/protected", methods=["GET"])
+def protected():
+    token = request.headers.get("Authorization")
+
+    if not token:
+        return jsonify({"error": "Token is missing"}), 401
+
+    try:
+        decoded = jwt.decode(token, app.config['SECRET_KEY'])
+        username = decoded['username']
+
+        return jsonify({"message": f"Hello, {username}! This is protected data."})
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({"error": "Token has expired"}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({"error": "Invalid token"}), 401
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Cloud Computing/user.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Bulan Mei 2023 pada 17.15
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database-api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(32) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`) VALUES
+(4, 'register2@smp.co.id', 'pbkdf2:sha256:600000$bokkSO4d6wF0a4Zm$d30cf83b2d9eaa0245e808c0d23be90a89780b35cf746abf2b6927de9277ab2c');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
